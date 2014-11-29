80490e7482d361b3c40fcc19643a9c9a197b433a
Tim Zallin <tim.zallin@gmail.com>
2014-11-29 10:23:04 +0400

    fixed an issue

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var cns,
 function main(){
 	width = window.innerWidth;
 	height = window.innerHeight;
-	var scale = ((width - 0.1 * width)/level.length)/75;
+	scale = 0.5;
 
 	cns = document.createElement('canvas');
 	cns.width = window.innerWidth;
@@ -26,7 +26,7 @@ function main(){
 	sprite.src = 'img/sprite.png';
 
 	sprite.onload = function () {
-		pieces = initPieces(this, 0.5);
+		pieces = initPieces(this, scale);
 		run();
 		//load level map
 		//run the game
@@ -71,17 +71,18 @@ function addListener(){
 		if(pressed){
 			evt.range.push({x : e.pageX, y : e.pageY});
 			if(evt.range.length === 10){
-				var inside = false;
 				var fx = evt.range[0].x;
 				var fy = evt.range[0].y;
 
 				for(var i = 0; i < balls.coordinates.length; i++){
-					var bx = coordinates[i].x;
-					var by = coordinates[i].y;
+					var bx = balls.coordinates[i].x;
+					var by = balls.coordinates[i].y;
 
-					var p1 = Math.min(Math.max(), );
-					var p2 = Math.min(Math.max(), )
-					if() inside = true;
+					var p1 = Math.min(Math.max(fx, bx), bx + 75*scale);
+					var p2 = Math.min(Math.max(fy, by), by + 75*scale);
+
+					if(p1 !== fx || p2 !== fy) return; 
+					console.log('inside'); 
 				}
 			}
 		}
