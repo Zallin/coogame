2c21205be7c596f6897835ab0f536ccf65112216
Tim Zallin <tim.zallin@gmail.com>
2014-12-06 16:49:48 +0400

    update method created

--- level.js
+++ level.js
@@ -20,9 +20,32 @@ function Level(m){
 
 	this.height = m.length;
 
-	this.update = function(){
+	this.update = function(x, y, dx, dy){
+		var lx = x - 0.2*width;
+		var ly = y - 0.2*width;
 
+		var ls = Math.floor(ly / (width * 0.6 / level.width));
+		var cs = Math.floor(lx / (width * 0.6 / level.width));
+
+		var l = ls;
+		var c = cs;
+
+		dx *= -1;
+		dy *= -1;
+
+		while(true){
+			l += dy;
+			c += dx;
+			if(level.map[l][c] !== ' ') break;
+		}
+
+		this.map[ls] = this.map[ls].replaceAt(cs, 'B');
+		this.map[l] = this.map[l].replaceAt(c, ' ');
 	}
 }
 
 var level = new Level(map);
+
+String.prototype.replaceAt = function(index, character){
+	return this.substr(0, index) + character + this.substr(index + character.length);
+}
