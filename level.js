bb332f60efa239934256822f78941d0a48ee8727
Tim Zallin <tim.zallin@gmail.com>
2014-12-06 09:45:25 +0400

    added level object

--- level.js
+++ level.js
@@ -1,8 +1,28 @@
-var level = [
-'XXEXXX',
-'XB   X',
-'X    E',
-'X    X',
-'XX B X',
-' XXXXX'
+var map = [
+	'XXEXXX',
+	'XB   X',
+	'X    E',
+	'X    X',
+	'XX B X',
+	' XXXXX'
 ];
+
+function Level(m){
+	this.map = map;
+
+	var w = m[0].length;
+
+	for(var i = 1; i < m.length; i++){
+		if(m[i].length > w) w = m[i].length;
+	}	
+
+	this.width = w;
+
+	this.height = m.length;
+
+	this.update = function(){
+
+	}
+}
+
+var level = new Level(map);
