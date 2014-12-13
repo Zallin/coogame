1f868a11b79f9ace5f0482a1d86727113befe3f9
Tim Zallin <tim.zallin@gmail.com>
2014-12-13 20:57:55 +0400

    updated

--- sprite.js
+++ sprite.js
@@ -1,8 +1,10 @@
-function Piece(x, y, width, height){
+function Piece(x, y, width, height, scale){
 	this.x = x;
 	this.y = y;
 	this.width = width;
 	this.height = height;
+
+	if(scale) this.scale = scale;
 }
 
 Piece.prototype.draw = function(ctx, x, y) {
@@ -30,6 +32,12 @@ function initPieces(img, scale){
 			new Piece(525, 0, 75, 75),
 			new Piece(600, 0, 75, 75),
 			new Piece(375, 0, 75, 75)
+		],
+
+		menu : [
+			new Piece(0, 75, 150, 42, 1),
+			new Piece(250, 75, 100, 54, 1),
+			new Piece(148, 75, 100, 54, 1)
 		]
 	}
 }
